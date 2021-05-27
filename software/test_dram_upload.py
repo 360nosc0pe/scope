bbec7ecc1c77ceff26923e1faaf94554c475be76
Florent Kermarrec <florent@enjoy-digital.fr>
2021-05-27 15:23:07 +0200

    software/test_dram_upload: Rewrite, now working correctly (issue was wrong UDPStreamer clock domain).

--- software/test_dram_upload.py
+++ software/test_dram_upload.py
@@ -16,35 +16,27 @@ from litex import RemoteClient
 bus = RemoteClient()
 bus.open()
 
-test_length = 0x10000
-
 class DRAMUpload:
-    def run(self, base, length):
-        for i in range(16):
-            bus.write(0x4000_0000 + 4*i, i)
-        #print(f"{bus.read(0x4000_0000):x}")
-        bus.regs.dma_reader_enable.write(0)
-        bus.regs.dma_reader_base.write(base)
-        bus.regs.dma_reader_length.write(length)
-        bus.regs.dma_reader_enable.write(1)
-        #while not (bus.regs.dma_reader_done.read() & 0x1):
-        #    print(bus.regs.dma_reader_offset.read())
-        #bus.regs.dma_reader_enable.write(0)
+    chunk_length = 1024
+    def run(self, base, length, filename="data.bin"):
+        with open(filename, "wb") as f:
+            # Create Socket and listen.
+            sock   = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind(("192.168.1.100", 2000))
 
-print("Start DRAM upload ...")
+            # Upload Data in chunks.
+            offset = 0
+            while length > 0:
+                bus.regs.dma_reader_enable.write(0)
+                bus.regs.dma_reader_base.write(base + offset)
+                bus.regs.dma_reader_length.write(self.chunk_length)
+                bus.regs.dma_reader_enable.write(1)
+                data, _ = sock.recvfrom(1024)
+                f.write(data)
+                length -= self.chunk_length
+                offset += self.chunk_length
+
 dram_upload = DRAMUpload()
-dram_upload.run(base=0x0000_0000, length=0x10000)
-
-print("Receive UDP data to data.bin...")
-data_length = 0
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("192.168.1.100", 2000))
-f = open("data.bin", "wb")
-while data_length < test_length:
-    print(f"{data_length}/{test_length}\r", end="")
-    data, addr = sock.recvfrom(1024)
-    f.write(data)
-    data_length += len(data)
-print("Done"+" "*16)
+data = dram_upload.run(base=0x0000_0000, length=0x100000)
 
 bus.close()
